6415d08d2737ed49ac61a463cb9fbb0596a8e929
Marco Brandizi <marco.brandizi@gmail.com>
2021-03-28 20:52:06 +0100

    SparqlEndPointHelper, dealing with disappearing transactions in long-running SPARQL processing.

--- rdfutils-jena/src/main/java/info/marcobrandizi/rdfutils/jena/SparqlEndPointHelper.java
+++ rdfutils-jena/src/main/java/info/marcobrandizi/rdfutils/jena/SparqlEndPointHelper.java
@@ -7,6 +7,7 @@ import java.util.function.Consumer;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.QuerySolutionMap;
 import org.apache.jena.rdf.model.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,13 +50,12 @@ public abstract class SparqlEndPointHelper
 		long[] ctr = { 0L };
 		
 		qx.execSelect ().forEachRemaining ( row -> {
-			// TODO: remove? Now it seems to be working again
-			/* Doing a clone after having observed transaction timeouts with TDB
+
+			// Doing a clone after having observed transaction timeouts with TDB
 			var clonedRow = new QuerySolutionMap ();
 			clonedRow.addAll ( row );
+			action.accept ( clonedRow );
 			
-			action.accept ( clonedRow );*/
-			action.accept ( row );
 			if ( ++ctr [ 0 ] % 100000 == 0 ) log.info ( "{}: {} SPARQL tuples read from RDF", logPrefix, ctr [ 0 ] ); 
 		});
 		
